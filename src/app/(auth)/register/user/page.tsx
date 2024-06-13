913b5b7b49be230796cfa4d3c0794842dca22ee6
mhdnazim <145525515+mhdnazim@users.noreply.github.com>
2024-06-13 11:07:49 +0530

    Update page.tsx

--- src/app/(auth)/register/user/page.tsx
+++ src/app/(auth)/register/user/page.tsx
@@ -111,7 +111,8 @@ const UserRegister = () => {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?explore)',
+            // backgroundImage: 'url(https://source.unsplash.com/random?explore)',
+            backgroundImage: 'url(https://picsum.photos/1200)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
