eadfe72f8ac5a7dbe7152c21e2ed0ac73eff255d
mhdnazim <145525515+mhdnazim@users.noreply.github.com>
2024-06-13 11:05:17 +0530

    Update Login.tsx

--- src/components/utils/Login.tsx
+++ src/components/utils/Login.tsx
@@ -210,7 +210,8 @@ const Login = () => {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?explore)',
+            // backgroundImage: 'url(https://source.unsplash.com/random?explore)',
+            backgroundImage: 'url(https://picsum.photos/1200)',
             backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
