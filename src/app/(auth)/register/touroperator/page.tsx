d5f67c3cbac33e24e84d2b49f9acff1c4d7e2a13
mhdnazim <145525515+mhdnazim@users.noreply.github.com>
2024-06-13 11:08:10 +0530

    Update page.tsx

--- src/app/(auth)/register/touroperator/page.tsx
+++ src/app/(auth)/register/touroperator/page.tsx
@@ -134,7 +134,8 @@ const TourOperatorRegister = () => {
             sm={4}
             md={7}
             sx={{
-              backgroundImage: 'url(https://source.unsplash.com/random?explore)',
+              // backgroundImage: 'url(https://source.unsplash.com/random?explore)',
+              backgroundImage: 'url(https://picsum.photos/1200)',
               backgroundRepeat: 'no-repeat',
               backgroundColor: (t) =>
                 t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
